1f98d05a89ca200e160a88c8ce0bc16b36f4894b
Mahmoud Saleh <ma7mouudbaky@gmail.com>
2025-08-31 16:59:31 +0300

    Remove deprecated environment configuration, Swagger setup, authentication routes, and user model files

--- dist/lib/constants.js
+++
@@ -1,5 +0,0 @@
-import { env } from "../config/env.js";
-export const MONGODB_URI = env.MONGODB_URI;
-export const PORT = env.PORT || 3000;
-export const BASE_URL = env.BASE_URL;
-//# sourceMappingURL=constants.js.map
